b3f532c661e1edb6a5bbc8f4ea994231e38e9e39
KHALID185 <khalid.1998.taouti@gmail.com>
2024-07-11 09:29:42 -0700

    airbnb clone v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.89.109.87', '100.25.190.21']
+env.hosts = ['34.227.93.84', '54.160.89.252']
 
 
 def do_deploy(archive_path):
