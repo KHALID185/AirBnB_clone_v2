ffe359ee316fbb8fb794f6736b030e823d09fdf2
KHALID185 <khalid.1998.taouti@gmail.com>
2024-07-11 09:31:08 -0700

    airbnb clone v2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import *
 from datetime import datetime
 import os
 
-env.hosts = ['54.144.136.64', '100.24.236.179']
+env.hosts = ['34.227.93.84', '54.160.89.252']
 env.user = 'ubuntu'
 
 
